5e1320d3c06effce0734c8eaef631851b7166e2e
Christopher Fitch <duke.fitch18@gmail.com>
2022-04-28 09:51:45 -0500

    Prevent default behavior of space / enter keys

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ currentMoney.innerHTML = "$ " + money;
 resetPuzzle( Object.values(phraseObject), insertPhrase, categoryDiv, hintElement, guessArr, currentMoney);
 
 // Reset puzzle once per day
-let dayInMilliseconds = 10000;
+let dayInMilliseconds = 100000;
 setInterval(resetPuzzle, dayInMilliseconds, Object.values(phraseObject), insertPhrase,
 categoryDiv, hintElement, guessArr, currentMoney, userGuessBtn, lettersArr);
 
@@ -41,6 +41,7 @@ enterBtn.addEventListener("click", enterLetter);
 
 // various key events
 document.addEventListener("keydown", function (event) {
+    event.preventDefault();
     if (event.key == "Enter") {
         enterLetter();
     }
